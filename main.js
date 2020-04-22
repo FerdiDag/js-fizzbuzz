0b0467ef34b54ffb83ae77634a9ffe975dc449a4
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-22 15:28:45 +0200

    forse finito fizzbuzz

--- main.js
+++ main.js
@@ -4,22 +4,27 @@
 // al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
 
 for (var i = 1; i < 101; i++) {
-   console.log (i);
 
 
     var fizz = (i % 3);
     var resto1 = fizz;
     var buzz = (i % 5);
     var resto2 = buzz;
+
+
     if (resto1 == 0 && resto2 == 0) {
 
         console.log('fizzbuzz');
+
     } else if (fizz == 0) {
 
         console.log('fizz');
     } else if (buzz == 0) {
 
         console.log('buzz');
+    } else {
+        console.log(i);
     }
+
 
 }
