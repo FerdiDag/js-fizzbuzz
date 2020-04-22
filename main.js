2049c2bc6fe3a942632a552b75292d0af8260cfe
Ferdinando <ferdi.dagostino@gmail.com>
2020-04-22 16:00:36 +0200

    esercizio corretto

--- main.js
+++ main.js
@@ -3,16 +3,10 @@
 // al posto dei multipli di 5 stampi "Buzz"
 // al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
 
-for (var i = 1; i < 101; i++) {
-
-
+for (var i = 1; i <= 100; i++) {
     var fizz = (i % 3);
-    var resto1 = fizz;
     var buzz = (i % 5);
-    var resto2 = buzz;
-
-
-    if (resto1 == 0 && resto2 == 0) {
+    if (fizz == 0 && buzz == 0) {
 
         console.log('fizzbuzz');
 
